e8364f606289446741587570c904f6eb66ae5159
Alex Toma <toma.alex.xd@gmail.com>
2023-04-05 11:52:08 +0300

    updated python serial simulator

--- python-serial/main.py
+++ python-serial/main.py
@@ -2,15 +2,19 @@ import serial
 
 
 def main():
-    print("started on COM8:")
     try:
-        ser = serial.Serial('COM8', 115200, serial.EIGHTBITS, serial.PARITY_EVEN, serial.STOPBITS_ONE, timeout=0)
+        port = "COM8"
+        ser = serial.Serial(port, 115200, serial.EIGHTBITS, serial.PARITY_EVEN, serial.STOPBITS_ONE, timeout=0)
+        
+        print(f"started on {port}:")
+        
         while True:
-            buffer = ser.read(100)
+            buffer = ser.read(8)
             if len(buffer) > 0:
                 print(f'read from serial: {buffer}')
-                ser.write(b'cevacevaceva')
+                ser.write(buffer)
                 print(f'wrote to serial')
+                
     except KeyboardInterrupt:
         quit(0)
 
